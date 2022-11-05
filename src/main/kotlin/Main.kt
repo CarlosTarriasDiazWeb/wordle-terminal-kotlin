b8d447ef1ee777fcf8087013c7ea0514517dd0fd
carlos.tarrias.7e6 <carlos.tarrias.7e6@itb.cat>
2022-11-05 12:57:31 +0100

    Corregido bug del número de intentos. Más comentarios. Controlada duplicidad de carácteres.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
-fun main(args: Array<String>) {
+fun main() {
+
+  //  Autor: Carlos Tarrias Diaz
+  //  Proyecto troncal M3 - Wordle
 
   //============DEFINICION E INICIALIZACION DEL ARRAY DE PALABRAS QUE TENDRA EL JUEGO
   val dictionary = arrayOf (
@@ -15,9 +18,9 @@ fun main(args: Array<String>) {
   )
 
   //============DEFINICION E INICIALIZACIÓN DE LOS COLORES PARA LAS LETRAS (CODIGO ANSI)=========//
-  val COLOUR_RESET = "\u001B[0m"
-  val COLOUR_GREEN = "\u001B[32m"
-  val COLOUR_YELLOW = "\u001B[33m"
+  val colourReset = "\u001B[0m"
+  val colourGreen = "\u001B[32m"
+  val colourYellow = "\u001B[33m"
 
   //================DEFINICION DE VARIABLES DE PARTIDA===============//
   var numTries: Int
@@ -25,18 +28,18 @@ fun main(args: Array<String>) {
   var guessWord: String
   var userLetter: String
   var correctLetters: Int
-  val letterToCount = mutableMapOf<Char, Int>()
+  var letterToCount = mutableMapOf<Char, Int>()
   var userOption: Int
   var randomIndex: Int
   var randomWord: String
 
-  //Aqui empieza bucle principal del juego
+  //Aqui empieza bucle principal del juego.
   do {
-    //Escogemos la palabra a partir del indice aleatorio
+    //Escogemos la palabra del diccionario a partir del indice aleatorio.
     randomIndex = dictionary.indices.random()
     randomWord = dictionary[randomIndex].uppercase()
 
-    //Creamos diccionario con ocurrencias de cada carácter para ver aquellos duplicados
+    //Creamos diccionario con ocurrencias de cada carácter para ver aquellos duplicados en cada intento.
     for (c in randomWord) {
       if (!letterToCount.containsKey(c)) {
         letterToCount[c] = 1
@@ -49,25 +52,30 @@ fun main(args: Array<String>) {
     println(letterToCount)//debug
     println(randomWord) //debug
 
-    //Reseteamos valores iniciales para la nueva partida
+    //Reseteamos valores iniciales para la nueva partida.
     numTries = 0
     playerWins = false
     println("Intenta adivinar la palabra (son 5 letras!) ")
 
-    //Aqui empieza el bucle de la partida
+    //Aqui empieza el bucle de la partida.
     do{
-      //Reseteamos valores iniciales para el nuevo intento
+      //Reseteamos valores iniciales para el nuevo intento.
       guessWord = ""
       correctLetters = 0
+      //Restauramos valores iniciales de ocurrencias de carácteres para el nuevo intento.
+      for (c in randomWord) {
+        letterToCount[c] = randomWord.count{it == c}
+      }
+      println(letterToCount) //debug
       println("Te quedan ${6 - (numTries)} intentos")
       repeat(5) { index->
         println("Introduce la letra ${index + 1}")
         userLetter = readln().uppercase()
-        while (userLetter.isEmpty()){
+        while (userLetter.isEmpty()){ //minetras el usuario no introduzca nada, vamos pidiendo la letra.
           println("Tines que escribir algo...")
           userLetter = readln().uppercase()
         }
-        if (userLetter.length > 1) {
+        if (userLetter.length > 1) { //para controlar que sólo re recoja un solo carácter cada vez que se lo pedimos al usuario.
           println("Has introducido mas de un carácter. Se recogerá solo el primero.")
           userLetter = userLetter.slice(0..0)
         }
@@ -75,12 +83,17 @@ fun main(args: Array<String>) {
         guessWord += userLetter
       }
       for (i in guessWord.indices) {
-        if (guessWord[i] in randomWord && guessWord[i] == randomWord[i]){
-          print("$COLOUR_GREEN${guessWord[i]}$COLOUR_RESET ")
+        //Para controlar el número de veces que aparece un carácter concreto en la palabra tenemos en cuenta su valor en el diccionario
+        if (guessWord[i] == randomWord[i] && letterToCount.getValue(guessWord[i]) > 0){
+          print("$colourGreen${guessWord[i]}$colourReset ")
           correctLetters++
+          //controlamos duplicidad del carácter
+          letterToCount[guessWord[i]] = letterToCount.getValue(guessWord[i]) - 1
         }
-        else if (guessWord[i] in randomWord) {
-          print("$COLOUR_YELLOW${guessWord[i]}$COLOUR_RESET ")
+        else if (guessWord[i] in randomWord && letterToCount.getValue(guessWord[i]) > 0) {
+          print("$colourYellow${guessWord[i]}$colourReset ")
+          //controlamos duplicidad del carácter
+          letterToCount[guessWord[i]] = letterToCount.getValue(guessWord[i]) - 1
         }
         else {
           print("${guessWord[i]} ")
@@ -89,26 +102,28 @@ fun main(args: Array<String>) {
       if (correctLetters == 5) {
         playerWins = true
         println("Enhorabuena has ganado!")
+        //Limpiamos diccionario de ocurrencias
+        letterToCount = mutableMapOf()
       }
       else {
         numTries++
-        if (numTries == 5) {
+        if (numTries == 6) {
           println("Una pena, no has acertado la palabra...")
         }
       }
       println("=================================")
-    }while(numTries < 5 && !playerWins) //seguimos intentando mientras no adivinemos la palabra y no hayamos superado el número máximo de intentos
+    }while(numTries < 6 && !playerWins) //seguimos intentando mientras no adivinemos la palabra y no hayamos superado el número máximo de intentos.
     println("Quieres hacer otra partida?")
     println("SI --> Pulsa 1")
     println("NO --> Pulsa 2")
     userOption = readln().toInt()
-    while (userOption < 1 || userOption > 2) {
+    while (userOption < 1 || userOption > 2) { //mientras la opción no sea correcta, vamos pidiéndole el input correcto al usuario.
       println("Opción no válida")
       println("SI --> Pulsa 1")
       println("NO --> Pulsa 2")
       userOption = readln().toInt()
     }
-  }while(userOption == 1) //seguimos jugando mientras el jugador así lo desee
+  }while(userOption == 1) //seguimos jugando mientras el jugador así lo indique.
   println("Apagando WORDLE...")
 
 }
